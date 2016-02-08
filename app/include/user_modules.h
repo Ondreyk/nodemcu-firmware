06e5e2e027569be4ab7a27e8f81557985cbd2580
Roman Chyla <roman.chyla@gmail.com>
2016-02-08 17:59:54 -0500

    gpioser: module for reading bits from sensors
    - use bit-banging to read sensors
    - added documentation

--- app/include/user_modules.h
+++ app/include/user_modules.h
@@ -24,6 +24,7 @@
 //#define LUA_USE_MODULES_ENDUSER_SETUP // USE_DNS in dhcpserver.h needs to be enabled for this module to work.
 #define LUA_USE_MODULES_FILE
 #define LUA_USE_MODULES_GPIO
+//#define LUA_USE_MODULES_GPIOSER
 #define LUA_USE_MODULES_HTTP
 //#define LUA_USE_MODULES_HX711
 #define LUA_USE_MODULES_I2C
@@ -46,7 +47,6 @@
 #define LUA_USE_MODULES_WIFI
 //#define LUA_USE_MODULES_WS2801
 #define LUA_USE_MODULES_WS2812
-
 
 #endif  /* LUA_CROSS_COMPILER */
 #endif	/* __USER_MODULES_H__ */
